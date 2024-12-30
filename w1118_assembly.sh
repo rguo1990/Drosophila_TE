aaa948f0a9dfe0d51190f3ad98b0afaaea08e097
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2024-12-30 12:36:51 -0800

    Update w1118_assembly.sh

--- w1118_assembly.sh
+++ w1118_assembly.sh
@@ -27,3 +27,27 @@ hifiasm -o w1118.asm -t32 --write-ec demultiplex.bc1017--bc1017.hifi_reads.fastq
 
 ## saved in: ~/Drosophila_genomes/w1118
 
+## the statistics of the primary contigs:
+## stats for /data/homezvol0/rongg7/Drosophila_genomes/w1118/w1118.asm.fasta
+## sum = 164268009, n = 193, ave = 851129.58, largest = 27963721
+## N50 = 21563735, n = 4
+## N60 = 15399632, n = 5
+## N70 = 7362488, n = 6
+## N80 = 1299991, n = 13
+## N90 = 429469, n = 32
+## N100 = 18114, n = 193
+## N_count = 0
+## Gaps = 0
+
+## stats for /data/homezvol0/rongg7/Drosophila_genomes/w1118/w1118.filter.asm.fasta
+## sum = 164601684, n = 206, ave = 799037.30, largest = 27963719
+## N50 = 21563737, n = 4
+## N60 = 15399646, n = 5
+## N70 = 7362488, n = 6
+## N80 = 1230914, n = 14
+## N90 = 383150, n = 35
+## N100 = 18385, n = 206
+## N_count = 0
+## Gaps = 0
+
+## If only considering the stats, it looks better when we don't filter the raw reads.
