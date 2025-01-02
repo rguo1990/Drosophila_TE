4e5469fcbc20377909dab2121b6ea84c4485f7ab
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2025-01-02 10:22:07 -0800

    Update w1118_assembly.sh

--- w1118_assembly.sh
+++ w1118_assembly.sh
@@ -50,4 +50,44 @@ hifiasm -o w1118.asm -t32 --write-ec demultiplex.bc1017--bc1017.hifi_reads.fastq
 ## N_count = 0
 ## Gaps = 0
 
-## If only considering the stats, it looks better when we don't filter the raw reads.
+## If only considering the stats, it looks better when we don't filter the raw reads, so we will take w1118.asm.fasta for future analysis.
+
+## Next, we did some filtering using GenomeFLTR (Dotan et al, 2023). Align the scaffolds against NCBI nr database, 
+## and filter out those mapped scaffolds with Kraken standard, (i.e. all complete bacterial, archeal, and viral genomes in Refseq)
+## The filtered 149 scaffolds were saved as: ~/Drosophila_genomes/w1118/assemblies/FilteredResults.fasta
+
+## Then, we performed reference-assisted assembly using RagTag.
+## install with conda (under environment ragtag_envs)
+## conda install -c bioconda ragtag
+
+ragtag.py scaffold ~/Drosophila_ref.fasta ~/Drosophila_genomes/w1118/assemblies/FilteredResult_relabel_geno.chr.fasta -o ~/Drosophila_genomes/Ragout/bin/ragtag_output
+
+head -n 14 ragtag.scaffold.fasta > w1118_ref.fasta
+## w1118_ref.fasta is the chromosomal-scale assembly of w1118, and each gap was filled with 100 Ns.
+
+## stats for /data/homezvol0/rongg7/Drosophila_genomes/Ragout/bin/ragtag_output/ragtag.scaffold.fasta
+## sum = 161781792, n = 98, ave = 1650834.61, largest = 34825399
+## N50 = 26933598, n = 3
+## N60 = 25937454, n = 4
+## N70 = 25937454, n = 4
+## N80 = 24106939, n = 5
+## N90 = 8181268, n = 6
+## N100 = 18114, n = 98
+## N_count = 5100
+## Gaps = 51
+
+## stats for ../../Ragout/bin/ragtag_output/w1118_ref.fasta
+## sum = 151137149, n = 7, ave = 21591021.29, largest = 34825399
+## N50 = 26933598, n = 3
+## N60 = 26933598, n = 3
+## N70 = 25937454, n = 4
+## N80 = 24106939, n = 5
+## N90 = 24106939, n = 5
+## N100 = 1867927, n = 7
+## N_count = 5100
+## Gaps = 51
+
+## The completeness of the w1118 assembly was measured using compleasm
+
+
+
