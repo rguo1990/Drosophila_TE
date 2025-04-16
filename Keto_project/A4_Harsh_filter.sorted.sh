af670638a361e04c897a331c192d48d7aa63b7e2
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2025-04-16 10:44:26 -0700

    Update A4_Harsh_filter.sorted.sh

--- Keto_project/A4_Harsh_filter.sorted.sh
+++ Keto_project/A4_Harsh_filter.sorted.sh
@@ -2,4 +2,6 @@
 ## bedextract <query.bed> <target>
 ## Grab elements from the <query.bed> that overlap elements in <target>
 cd ~/Keto/TEMP2/rmsk/
-bedextract A4_A7.UU_MU.Zscore.SigFlag.txt A4_Harsh.sorted.bed > A4_Harsh_filter.sorted.bed
+bedextract A4.sorted.bed A4_Harsh.sorted.bed > A4_Harsh_filter.sorted.bed
+
+## A4.sorted.bed is RepeatMasker's TE calling, in bed6 format (compatible with TEMP2) 
